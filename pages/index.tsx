37f10979cb564e5def64ac3c3be86bb7d74320ee
Vinicius <viniciusff200@gmail.com>
2025-04-11 17:14:21 -0300

    Terminando a seção de apresentação

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import HeaderNoAuth from '../src/components/homeNoAuth/headerNoAuth';
 import PresentationSection from '../src/components/homeNoAuth/presentationSection';
+import styles from '../styles/HomeNoAuth.module.scss';
 
 
 const HomeNoAuth = () => {
@@ -10,11 +11,13 @@ const HomeNoAuth = () => {
         <title>Onebitflix</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         <meta property='og:title' content='Onebitflix' key="title" />
-        <meta name='description' content='Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!'/>
+        <meta name='description' content='Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!' />
       </Head>
       <main>
-        <HeaderNoAuth/>
-        <PresentationSection />
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
       </main>
     </>
   );
