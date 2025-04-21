ba997fce67fcc4109a9c037471913a984dd55f68
Vinicius <viniciusff200@gmail.com>
2025-04-21 16:08:57 -0300

    Finalizando a seção dos slides, visual e também responsividade

--- src/components/homeNoAuth/slideSection/index.tsx
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -10,7 +10,7 @@ interface props {
 
 const SlideSection = ({newestCourses}: props) => {
     return <>
-        <Container>
+        <Container className="d-flex flex-column align-items-center py-5">
             <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
             <SlideComponent course={newestCourses}/>
             <Link href='/register'><Button className={styles.slideSectionBtn} outline color='light'>Se cadastre para acessar!</Button></Link>
