a6206fd2739db7edf3652483711c0b8e03de8681
Vinicius <viniciusff200@gmail.com>
2025-04-23 14:29:40 -0300

    Adicionando animação na pagina homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,24 @@ import CardSection from '../src/components/homeNoAuth/cardsSection';
 import SlideSection from '../src/components/homeNoAuth/slideSection';
 import { GetStaticProps } from 'next';
 import courseService, { CourseType } from '../src/services/courseService';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import Footer from '../src/components/common/footer';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 
-interface IndexPageProps{
+interface IndexPageProps {
   children?: ReactNode;
   course: CourseType[];
 
 }
 
-const HomeNoAuth = ({course}: IndexPageProps) => {
+const HomeNoAuth = ({ course }: IndexPageProps) => {
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
     <>
       <Head>
@@ -26,19 +33,23 @@ const HomeNoAuth = ({course}: IndexPageProps) => {
         <meta name='description' content='Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!' />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div className={styles.sectionBackground} data-aos="fade-zoom-in" data-aos-duration="1600">
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardSection />
-        <SlideSection newestCourses={course}/>
-        <Footer/>
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={course} />
+        </div>
+        <Footer />
       </main>
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps = async() => {
+export const getStaticProps: GetStaticProps = async () => {
   const res = await courseService.getNewestCourses();
   return {
     props: {
